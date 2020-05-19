018d36208b61b81df24806716866f74b9df72c94
ThishenP <Thishenp@gmail.com>
2020-05-19 21:15:27 +0200

    added damaging walls

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject damageWallPrefab;
+    public GameObject wallPrefab;
+    public float wallThresholdRate;
     
     private int amountOfPrefabs = 10;
     private GameObject[] enemy;
+    private GameObject[] damageWall;
+    private GameObject[] wall;
     private Vector2 poolPos = new Vector2(-40,-40);
     private Vector2 spawnPos;
     private float timeSinceSpawn=0;
+    private float wallChangeTime = 0;
     private int enemyIndex=0;
+    private float wallThreshold=100;
     
     // Start is called before the first frame update
     void Start()
@@ -22,13 +29,40 @@ public class EnemySpawner : MonoBehaviour
         {
            enemy[i] = Instantiate(enemyPrefab, poolPos, Quaternion.identity);
         }
+        
+
+        damageWall = new GameObject[4];
+        damageWall[0] = Instantiate(damageWallPrefab, new Vector2(9.9f, 0), Quaternion.identity);
+        damageWall[1] = Instantiate(damageWallPrefab, new Vector2(-9.9f, 0), Quaternion.identity);
+        damageWall[2] = Instantiate(damageWallPrefab, new Vector2(0, 6), Quaternion.identity);
+        damageWall[3] = Instantiate(damageWallPrefab, new Vector2(0, -6), Quaternion.identity);
+        damageWall[2].transform.Rotate(new Vector3(0,0,90));
+        damageWall[3].transform.Rotate(new Vector3(0, 0, 90));
+        for(int i =0; i < 4; i++)
+        {
+            damageWall[i].SetActive(false);
+        }
+
+        wall = new GameObject[4];
+        wall[0] = Instantiate(wallPrefab, new Vector2(9.9f, 0), Quaternion.identity);
+        wall[1] = Instantiate(wallPrefab, new Vector2(-9.9f, 0), Quaternion.identity);
+        wall[2] = Instantiate(wallPrefab, new Vector2(0, 6), Quaternion.identity);
+        wall[3] = Instantiate(wallPrefab, new Vector2(0, -6), Quaternion.identity);
+        wall[2].transform.Rotate(new Vector3(0, 0, 90));
+        wall[3].transform.Rotate(new Vector3(0, 0, 90));
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (wallThreshold>45)
+        {
+            wallThreshold -= Time.deltaTime * wallThresholdRate;
+        }
+        
         timeSinceSpawn += Time.deltaTime;
+        wallChangeTime += Time.deltaTime;
         if (timeSinceSpawn>2 && control.instance.end==false)
         {
             spawnPos = RandomEnemySpawnPoint();
@@ -41,6 +75,58 @@ public class EnemySpawner : MonoBehaviour
                 enemyIndex = 0;
             }
         }
+        if (wallChangeTime>4 && control.instance.end==false)
+        {
+            Debug.Log(wallThreshold);
+            float wall0 = Random.Range(1, 101);
+            float wall1 = Random.Range(1, 101);
+            float wall2 = Random.Range(1, 101);
+            float wall3 = Random.Range(1, 101);
+            Debug.Log(wall0);
+            if (wall0 > wallThreshold)
+            {
+                wall[0].SetActive(false);
+                damageWall[0].SetActive(true);
+            }
+            else
+            {
+                wall[0].SetActive(true);
+                damageWall[0].SetActive(false);
+            }
+            if (wall1 > wallThreshold)
+            {
+                wall[1].SetActive(false);
+                damageWall[1].SetActive(true);
+            }
+            else
+            {
+                wall[1].SetActive(true);
+                damageWall[1].SetActive(false);
+            }
+            if (wall2 > wallThreshold)
+            {
+                wall[2].SetActive(false);
+                damageWall[2].SetActive(true);
+            }
+            else
+            {
+                wall[2].SetActive(true);
+                damageWall[2].SetActive(false);
+            }
+            if (wall3 > wallThreshold)
+            {
+                wall[3].SetActive(false);
+                damageWall[3].SetActive(true);
+            }
+            else
+            {
+                wall[3].SetActive(true);
+                damageWall[3].SetActive(false);
+            }
+            wallChangeTime = 0;
+        }
+
+
     }
 
     Vector2 RandomEnemySpawnPoint()
@@ -66,4 +152,9 @@ public class EnemySpawner : MonoBehaviour
         return spawnPoint;
         
     }
+
+
+    
+
+
 }
--- Assets/playerHealth.cs
+++ Assets/playerHealth.cs
@@ -10,12 +10,15 @@ public class playerHealth : MonoBehaviour
     private float health = 100;
     private Vector2 poolPos = new Vector2(-40, -40);
     public GameObject particlePrefab;
+    public GameObject smallExplostionPrefab;
+    private GameObject smallExplosion;
     private GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
         healthBar.fillAmount = 1;
         explosion = Instantiate(particlePrefab, poolPos, Quaternion.identity);
+        smallExplosion = Instantiate(smallExplostionPrefab, poolPos, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -31,6 +34,13 @@ public class playerHealth : MonoBehaviour
 
             if (invincibility == false) { health -= 10; }
             healthBar.fillAmount = health / 100;
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            if (invincibility == false) { health -= 20; }
+            healthBar.fillAmount = health / 100;
+            smallExplosion.transform.position = transform.position;
+            smallExplosion.GetComponent<ParticleSystem>().Play();
         }
 
         if (health <= 0)
